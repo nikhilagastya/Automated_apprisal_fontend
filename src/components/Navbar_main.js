2e39c4c610406595ebebc33b4b95646cef47155a
Nikhil <karthikagastya2002@gmail.com>
2024-01-02 13:40:50 +0530

    navigator

--- src/components/Navbar_main.js
+++ src/components/Navbar_main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import { Navbar, Nav } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar_main = () => {
   let info = localStorage.getItem("Details");
   info = JSON.parse(info);
-  
+  const navigate = useNavigate();
   return (
 
    
@@ -14,22 +15,22 @@ const Navbar_main = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-       <button onClick={()=>{navigator("/home")}}> <Nav.Link style={{ color: "black", fontWeight: "bold" }} >
+       <button onClick={()=>{navigate("/home")}}> <Nav.Link style={{ color: "black", fontWeight: "bold" }} >
             Profile
           </Nav.Link>
           </button>
-          <button onClick={()=>{navigator("/home")}}>
+          <button onClick={()=>{navigate("/home")}}>
           <Nav.Link style={{ color: "black", fontWeight: "bold" }} href="/api_form">
             API Form
           </Nav.Link>
           </button>
-          <button onClick={()=>{navigator("/home")}}>
+          <button onClick={()=>{navigate("/home")}}>
           <Nav.Link style={{ color: "black", fontWeight: "bold" }} href="/status">
             Status
           </Nav.Link>
           </button>
           {info.designation === "HOD" ? (
-             <button onClick={()=>{navigator("/home")}}>
+             <button onClick={()=>{navigate("/home")}}>
             <Nav.Link style={{ color: "black", fontWeight: "bold" }} href="/approvals">
               Approvals
             </Nav.Link>
